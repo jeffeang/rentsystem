23a630934c654bdd99f9fe10719b0c56d9aa5c89
jeffeang <wangxiaoming78@gmail.com>
2021-09-27 21:05:28 -0400

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LoginForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.UserInput = New System.Windows.Forms.TextBox()
+        Me.PwdInput = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.LoginBt = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Sitka Display", 28.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(147, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(469, 69)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Système ChromeBook"
+        '
+        'UserInput
+        '
+        Me.UserInput.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.UserInput.Location = New System.Drawing.Point(336, 151)
+        Me.UserInput.Name = "UserInput"
+        Me.UserInput.Size = New System.Drawing.Size(197, 31)
+        Me.UserInput.TabIndex = 1
+        '
+        'PwdInput
+        '
+        Me.PwdInput.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.PwdInput.Location = New System.Drawing.Point(336, 218)
+        Me.PwdInput.Name = "PwdInput"
+        Me.PwdInput.Size = New System.Drawing.Size(197, 31)
+        Me.PwdInput.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(160, 154)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(160, 25)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Nom d'utilisateur"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(170, 221)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(130, 25)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Mot de  Passe"
+        '
+        'LoginBt
+        '
+        Me.LoginBt.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.LoginBt.Location = New System.Drawing.Point(301, 298)
+        Me.LoginBt.Name = "LoginBt"
+        Me.LoginBt.Size = New System.Drawing.Size(122, 38)
+        Me.LoginBt.TabIndex = 5
+        Me.LoginBt.Text = "Login"
+        Me.LoginBt.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.LoginBt.UseVisualStyleBackColor = True
+        '
+        'LoginForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LoginBt)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PwdInput)
+        Me.Controls.Add(Me.UserInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "LoginForm"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents UserInput As TextBox
+    Friend WithEvents PwdInput As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents LoginBt As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Data.SqlClient
+
+Public Class LoginForm
+
+    Private Sub LoginBt_Click(sender As Object, e As EventArgs) Handles LoginBt.Click
+        Dim User = UserInput.Text
+        Dim Paswd = PwdInput.Text
+
+        Using con As SqlConnection = New SqlConnection("Data Source=DELL-XIAOMING\SQLEXPRESS;Initial Catalog=rentsystem;Integrated Security=Tru")
+            con.Open()
+            Dim commande As New SqlCommand("select * from admin_user where user_name =@username and password=@pwd ", con)
+        End Using
+
+
+    End Sub
+End Class
