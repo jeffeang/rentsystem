ec214e53c71f9b447a82bef0ca2f3261ab89c606
jeffeang <wangxiaoming78@gmail.com>
2021-09-27 21:05:28 -0400

    chance 2Ajoutez des fichiers projet.

---
+++ Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Data.SqlClient
+
+Public Class LoginForm
+
+    Private Sub LoginBt_Click(sender As Object, e As EventArgs) Handles LoginBt.Click
+        Dim User = UserInput.Text
+        Dim Paswd = PwdInput.Text
+
+        Using con As SqlConnection = New SqlConnection("Data Source=DELL-XIAOMING\SQLEXPRESS;Initial Catalog=rentsystem;Integrated Security=Tru")
+            con.Open()
+            Dim commande As New SqlCommand("select count(*) as rows from admin_user where user_name =@username and password=@pwd ", con)
+            commande.Parameters.AddWithValue("@username", User)
+            commande.Parameters.AddWithValue("@pwd", Paswd)
+            Using reader As SqlDataReader = commande.ExecuteReader
+                If reader.Read Then
+                    Dim row = reader.GetValue("rows").ToString
+                    If (row > 0) Then
+
+                    End If
+                End If
+            End Using
+        End Using
+
+
+    End Sub
+End Class
